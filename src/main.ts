90039d4a53b35a5cb28ef822387cb4dde3d3e266
Elham-EN <ilhamrezaie@gmail.com>
2022-09-23 23:15:28 +1000

    Successfully added user to the database

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //Validate any incoming request from the client
+  //Validate any incoming request from the client. The whitelist property
+  //to make sure that incoming requests don't have unrelated properties that
+  //we are not expecting. This is because of security reason we don't want to
+  //allow users to add in additional properties in the incoming request.
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+//Hook up the User Repository to the User Service
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  /**
+   * First the argument name is repo, the type annotation Repository and applied
+   * generic type of User. This means that repo is going to be an instance of a
+   * typeORM Repository that deals with instances of user. The InjectRepository
+   * is going to tell the dependency injection system that we need user repository.
+   * Use this anytime when you need a typeORM repository.
+   */
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  //Create User and save to the database
+  create(email: string, password: string) {
+    //Create instance of User entity
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+}
