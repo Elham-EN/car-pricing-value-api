6ed76d50cde193bd1f1e512cb49c67e21f6cbd5e
Elham-EN <ilhamrezaie@gmail.com>
2022-09-23 20:29:36 +1000

    Created Entity classes

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  // Create the repository (Connect entity to it's parent module)
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
